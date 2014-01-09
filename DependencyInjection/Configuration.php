1e3764cf90b57ac0fd01398876fc01afd1da6d3d
Patrik Patie Gmitter <patie@patie.info>
2014-01-09 20:54:52 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace cayetanosoriano\HashidsBundle\DependencyInjection;
+namespace Cayetanosoriano\HashidsBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
